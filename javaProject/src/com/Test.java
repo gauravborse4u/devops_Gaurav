362ed2e7b0d1dea5421474345a051777ce6baf9b
gauravborse4u <gauravborse4u@gmail.com>
2022-10-11 11:14:45 +0530

    commit

--- javaProject/src/com/Test.java
+++ javaProject/src/com/Test.java
@@ -2,8 +2,13 @@ package com;
 
 public class Test {
 
-	void show() {
-		System.out.println("Hello World!");
+	void show(int a, int b) {
+		System.out.println(a+b);
+	}
+	
+	public static void main(String args[]) {
+		Test t = new Test();
+		t.show(55, 55);
 	}
 	
 }
