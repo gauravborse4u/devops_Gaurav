89561360172e32d4be89160907b9a6e0340a766c
gauravborse4u <gauravborse4u@gmail.com>
2022-10-14 19:39:39 +0530

    commit

--- javaProject/src/com/Test.java
+++ javaProject/src/com/Test.java
@@ -11,14 +11,5 @@ public class Test {
 		Test t=new Test();
 		t.add(44, 55);
 	}
-
-	void show(int a, int b) {
-		System.out.println(a+b);
-	}
-	
-	public static void main(String args[]) {
-		Test t = new Test();
-		t.show(55, 55);
-	}
 	
 }
